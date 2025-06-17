280e879fef909d1e0721473e942cb1fe1fae9431
CLAM <>
2025-06-17 09:32:02 +0200

    add health check and explorer links

--- src/components/Header/Polling.tsx
+++ src/components/Header/Polling.tsx
@@ -6,7 +6,6 @@ import { useActiveNetworkVersion, useSubgraphStatus } from '../../state/applicat
 import { ExplorerDataType, getExplorerLink } from '../../utils'
 import useTheme from 'hooks/useTheme'
 import { EthereumNetworkInfo } from 'constants/networks'
-import { ChainId } from '@uniswap/sdk-core'
 
 const StyledPolling = styled.div`
   display: flex;
@@ -86,9 +85,7 @@ export default function Polling() {
   )
 
   return (
-    <ExternalLink
-      href={latestBlock ? getExplorerLink(ChainId.MAINNET, latestBlock.toString(), ExplorerDataType.BLOCK) : ''}
-    >
+    <ExternalLink href={latestBlock ? getExplorerLink(999, latestBlock.toString(), ExplorerDataType.BLOCK) : ''}>
       <StyledPolling>
         <TYPE.small mr="4px" color={theme?.text3}>
           Latest synced block:{' '}
